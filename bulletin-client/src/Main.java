548781ad8dd5da172467405f5068d30621c0967e
Robbe Hovaert <robbe.hovaert@gmail.com>
2024-11-11 11:38:04 +0100

    initial commit

---
+++ bulletin-client/src/Main.java
@@ -0,0 +1,9 @@
+/**
+* Default (Template) Project: ${NAME}
+* @author robbe
+* @version 11/11/2024
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
